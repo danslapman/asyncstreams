719ce0737e3ff7d40bb9e0acb902d35d2bba0e8e
Daniil Smirnov <danslapman@gmail.com>
2018-06-17 15:03:26 +0300

    Re-format file

--- src/main/scala/asyncstreams/package.scala
+++ src/main/scala/asyncstreams/package.scala
@@ -19,7 +19,8 @@ package object asyncstreams {
     def withFilter(f: A => Boolean): F[A] = fa.filter(f)
   }
 
-  implicit def streamInstance[F[_]: λ[`x[_]` => MonadError[x, Throwable]]]: Monad[AsyncStream[F, ?]] with Alternative[AsyncStream[F, ?]] = new ASInstanceForMonadError[F]
+  implicit def streamInstance[F[_]: λ[`x[_]` => MonadError[x, Throwable]]]: Monad[AsyncStream[F, ?]] with Alternative[AsyncStream[F, ?]] =
+    new ASInstanceForMonadError[F]
   implicit def asimpl[F[_]: λ[`x[_]` => MonadError[x, Throwable]]]: ASImpl[F] = new ASImplForMonadError[F]
   implicit def zeroK[F[_]](implicit me: MonadError[F, Throwable]): EmptyK[F] = new EmptyK[F] {
     override def empty[A]: F[A] = me.raiseError(new NoSuchElementException)
